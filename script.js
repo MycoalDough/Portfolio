e895b9638dc0edd4709301666362c1f314719de7
Michael Do <mycoaldough@gmail.com>
2024-04-20 02:03:18 -0700

    update

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function spawnCloud() {
 
     cloud.style.left = "-100px";
     cloud.style.top = getRandomNumber(0, window.innerHeight - 200) + "px";
-    cloud.style.opacity = "0.5"; 
+    cloud.style.opacity = getRandomNumber(0,0.1).toString(); 
     
     var size = getRandomNumber(50, 150);
     cloud.style.width = size + "px";
@@ -19,7 +19,7 @@ function spawnCloud() {
 
     // Animation
     var speed = getRandomNumber(20, 100); 
-    var endPosition = window.innerWidth + 100;
+    var endPosition = window.innerWidth - 10;
     var interval = setInterval(function() {
         var currentPosition = parseFloat(cloud.style.left);
         if (currentPosition >= endPosition) {
@@ -31,4 +31,62 @@ function spawnCloud() {
     }, 1000 / 60);
 }
 
-setInterval(spawnCloud, getRandomNumber(2000,15000));
+setInterval(spawnCloud, getRandomNumber(10000,20000));
+
+function filterProjects() {
+    var checkbox1 = document.getElementById("checkbox1");
+    var checkbox2 = document.getElementById("checkbox2");
+    var projects = document.querySelectorAll('.project');
+
+    if (checkbox1.checked && !checkbox2.checked) {
+        projects.forEach(function(project) {
+            if (project.id === "AI") {
+                project.style.display = "block";
+            } else {
+                project.style.display = "none";
+            }
+        });
+    } else if (!checkbox1.checked && checkbox2.checked) {
+        projects.forEach(function(project) {
+            if (project.id === "GAME") {
+                project.style.display = "block";
+            } else {
+                project.style.display = "none";
+            }
+        });
+    } else {
+        projects.forEach(function(project) {
+            project.style.display = "block";
+        });
+    }
+}
+
+
+const letters = "qwertyuiopasdfghjklzxcvbnm234567890!@#$%^&*()";
+
+let interval = null;
+
+document.getElementById("main_menu_p").addEventListener("mouseover", event => {
+      let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 40)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 1);
+});
